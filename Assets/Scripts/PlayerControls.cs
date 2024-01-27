38ea886af42b58cf29cdd1703252bb7f0633a8cc
shiroxkuro77 <103314294+shiroxkuro77@users.noreply.github.com>
2024-01-27 08:47:17 +0800

    Update Movement and Add sample asssets

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 public class PlayerControls : MonoBehaviour
-{
-    public float movSpeed;
-    float speedX, speedY;
+{   
+    public float moveSpeed = 1f;
+    public float collisionOffset = 0.05f;
+    public ContactFilter2D movementFilter;
+    Vector2 movementInput;
     Rigidbody2D rb;
+    List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
     // Start is called before the first frame update
     void Start()
@@ -14,11 +18,23 @@ public class PlayerControls : MonoBehaviour
         rb = GetComponent<Rigidbody2D>();    
     }
 
+    private void FixedUpdate() {
+        if (movementInput != Vector2.zero) {
+            int count = rb.Cast(
+                movementInput,
+                movementFilter,
+                castCollisions,
+                moveSpeed * Time.fixedDeltaTime + collisionOffset);
+
+            if (count == 0) {
+                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+            }
+        }
+    }
+
     // Update is called once per frame
-    void Update()
+    void OnMove(InputValue movementValue)
     {
-        speedX = Input.GetAxisRaw("Horizontal") * movSpeed;
-        speedY = Input.GetAxisRaw("Vertical") * movSpeed;
-        rb.velocity = new Vector2(speedX, speedY);
+        movementInput = movementValue.Get<Vector2>();
     }
 }
