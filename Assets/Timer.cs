ef10a8b6aecf5dcca8466809626850ae2ffd82a5
Wong Da You <64577096+wongdayou@users.noreply.github.com>
2024-01-27 16:13:16 +0800

    HB-23 Basic Timer functionality
    Remaining part to do is to activate game over functionality when timer reaches zero.

---
+++ Assets/Timer.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Timer : MonoBehaviour
+{
+    [SerializeField] private float timeLeft = 30.0f;
+    [SerializeField] private TextMeshProUGUI timeText;
+    [SerializeField] private float timeToTurnRed = 10f;
+    public bool gameStarted = false;
+    private float timeToDisplay;
+
+    public Color warningColor;
+
+    private bool turnedRed = false;
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (gameStarted && timeLeft > 0){
+            timeLeft = Mathf.Clamp(timeLeft - Time.deltaTime, 0, 100);
+            timeToDisplay = Mathf.Ceil(timeLeft);
+            if (timeToDisplay <= timeToTurnRed && !turnedRed){
+                timeText.color = warningColor;
+            }
+            timeText.text = timeToDisplay.ToString();
+        }
+    }
+
+    public void SetTimer(float time){
+        timeLeft = time;
+    }
+
+    public void StartTimer(){
+        gameStarted = true;
+    }
+
+    //TODO notify gamemaster when time is up
+    public void TimeUp(){
+        // call gamemaster component and end game
+    }
+}
