1c865b013b2f2332ed59afa1056971305b5c5270
shiroxkuro77 <103314294+shiroxkuro77@users.noreply.github.com>
2024-01-27 09:11:55 +0800

    Flip Direction

--- Assets/Scripts/PlayerControls.cs
+++ Assets/Scripts/PlayerControls.cs
@@ -9,27 +9,51 @@ public class PlayerControls : MonoBehaviour
     public float collisionOffset = 0.05f;
     public ContactFilter2D movementFilter;
     Vector2 movementInput;
+    SpriteRenderer spriteRenderer;
     Rigidbody2D rb;
     List<RaycastHit2D> castCollisions = new List<RaycastHit2D>();
 
     // Start is called before the first frame update
     void Start()
     {
-        rb = GetComponent<Rigidbody2D>();    
+        rb = GetComponent<Rigidbody2D>(); 
+        spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
     private void FixedUpdate() {
         if (movementInput != Vector2.zero) {
-            int count = rb.Cast(
-                movementInput,
-                movementFilter,
-                castCollisions,
-                moveSpeed * Time.fixedDeltaTime + collisionOffset);
+           bool success = TryMove(movementInput);
 
-            if (count == 0) {
-                rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
-            }
+           if (!success) {
+                success = TryMove(new Vector2(movementInput.x, 0));
+
+                if (!success) {
+                    success = TryMove(new Vector2(0, movementInput.y));
+                }
+           }
         }
+
+        if (movementInput.x < 0) {
+            spriteRenderer.flipX = false;
+        } else if (movementInput.x > 0) {
+            spriteRenderer.flipX = true;
+        }
+    }
+
+    private bool TryMove(Vector2 direction) {
+        int count = rb.Cast(
+                        movementInput,
+                        movementFilter,
+                        castCollisions,
+                        moveSpeed * Time.fixedDeltaTime + collisionOffset);
+
+        if (count == 0) {
+            rb.MovePosition(rb.position + movementInput * moveSpeed * Time.fixedDeltaTime);
+            return true;
+        } else {
+            return false;
+        }
+
     }
 
     // Update is called once per frame
