9ed015a12e9b28721ca277471d8679940458817a
shiroxkuro77 <103314294+shiroxkuro77@users.noreply.github.com>
2024-01-28 11:14:04 +0800

    Add Cat NPC

--- Assets/Scripts/QuestTrigger.cs
+++ Assets/Scripts/QuestTrigger.cs
@@ -2,13 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using Ink.Runtime;
     
 public class QuestTrigger : MonoBehaviour
 {
 
     [SerializeField] private GameObject visualCue;
+
+    [Header("Ink JSON")]
+    [SerializeField] private TextAsset inkJSON;
     private bool playerInRange;
 
+    private Story currentStory;
 
     private void Awake() {
         playerInRange = false;
@@ -16,11 +21,33 @@ public class QuestTrigger : MonoBehaviour
     }
 
     private void Update() {
+
+        if (DialogueManager.GetInstance().dialogueIsPlaying) {
+            return;
+        }
+
         if (playerInRange) {
             visualCue.SetActive(true);
+            
+            if (Keyboard.current.FindKeyOnCurrentKeyboardLayout("f").wasPressedThisFrame) {
+                currentStory = new Story(inkJSON.text);
+                DialogueManager.GetInstance().EnterDialogueMode(currentStory);
+            }
         } else {
             visualCue.SetActive(false);
         }
+
+        if (currentStory == null) {
+            return;
+        }
+        
+        int catsaved = int.Parse(currentStory.variablesState["catsaved"].ToString());
+
+        if (catsaved == 1) {
+            GameObject parentObject = transform.parent.gameObject;
+            parentObject.SetActive(false);
+        }
+
     }
 
     void OnTriggerEnter2D(Collider2D collision)
